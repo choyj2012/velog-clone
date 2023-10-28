769dacf70edf3f03ec2102bee0f181a35751026e
choyj2012 <choyj2012@gmail.com>
2023-10-28 16:47:46 +0900

    댓글 ui 추가

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -1,15 +1,17 @@
-import { Suspense, useEffect, useState } from "react"
-import { useNavigate, useParams } from "react-router"
-import { deletePost, getPost } from "../../firebase";
+import { useEffect, useState } from "react"
+import { useParams } from "react-router"
+import { getPost } from "../../firebase";
 import styled from "styled-components";
 import Header from "../component/Header";
 import MDEditor from "@uiw/react-md-editor"
 
 export default function Post() {
   const param = useParams();
-  const [postData, setPostData] = useState();
+  const [postData, setPostData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
-  const navigate = useNavigate();
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState('');
+
   useEffect(() => {
     let ignore = false;
     async function fetchData() {
@@ -25,6 +27,12 @@ export default function Post() {
       ignore = true;
     }
   }, [])
+
+  const handleTextAreaHeight = (e) => {
+    setComment(e.target.value);
+    e.target.style.height = '0px';
+    e.target.style.height = e.target.scrollHeight + 'px';
+  }
   return (
     <>
       <Header />
@@ -49,9 +57,24 @@ export default function Post() {
               value={postData.content}
               preview="preview"
               hideToolbar={true}
+              visibleDragbar={false}
+              height={'auto'}
             />
+            <CommentWrapper>
+              <h2>{postData.commentsCnt} Comments</h2>
+              <CommentTextArea onChange={handleTextAreaHeight} value={comment}/>
+              <AddCommentBtn>댓글 작성</AddCommentBtn>
+              
+              <div style={{
+                height: '3px', backgroundColor: 'black', 
+                marginBottom: '1rem', marginTop: '1rem'}}></div>
+              <div>
+                {postData.comment.map((com) => {
+                  return <div>- {com}</div>;
+                })}
+              </div>
+            </CommentWrapper>
           </PostWrapper>
-          {/* <PostContent postData={postData} /> */}
         </>
       )}
     </>
@@ -67,6 +90,28 @@ const PostWrapper = styled.div`
     width: 100%;
   }
 `
+
+const CommentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 5rem;
+`
+const CommentTextArea = styled.textarea`
+  resize: none;
+  font-size: 1rem;
+  line-height: 1.75;
+  border-radius: 4px;
+  padding: 1rem;
+`
+
+const AddCommentBtn = styled.button`
+  font-size: 1rem;
+  align-self: flex-end;
+  width: 100px;
+  margin-top: 1rem;
+  padding: 5px;
+`
+
 function Loading(){
   return (
     <div>
